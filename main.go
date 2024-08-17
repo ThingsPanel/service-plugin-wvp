d2f73dcbbede285474023792a3e178038a88a02c
wangzongchun <zongchunw@gmail.com>
2024-08-17 10:45:54 +0800

    feat

--- main.go
+++ main.go
@@ -1,14 +1,10 @@
 package main
 
 import (
-	"github.com/sirupsen/logrus"
-	"golang.org/x/net/context"
 	"log"
-	"plugin_wvp/apis"
 	"plugin_wvp/cache"
 	httpclient "plugin_wvp/http_client"
 	httpservice "plugin_wvp/http_service"
-	"plugin_wvp/model"
 	"plugin_wvp/mqtt"
 	"plugin_wvp/services"
 	"strings"
@@ -22,19 +18,19 @@ func main() {
 	LogInIt()
 	log.Println("Starting the application...")
 
-	apiToken := "eyJhbGciOiJSUzI1NiIsImtpZCI6IjNlNzk2NDZjNGRiYzQwODM4M2E5ZWVkMDlmMmI4NWFlIn0.eyJqdGkiOiJOZzZPR3BPeG03SGhhbzl6NlJYMmtRIiwiaWF0IjoxNzIzNzI4MzYxLCJleHAiOjE4NjE4OTExNDUsIm5iZiI6MTcyMzcyODM2MSwic3ViIjoibG9naW4iLCJhdWQiOiJBdWRpZW5jZSIsInVzZXJOYW1lIjoiYWRtaW4iLCJhcGlLZXlJZCI6MX0.ZkYCr1ffxij8wBHMCSJIqkeYoSAwe2ko-vVsnwi60SVxPA6lTosrv3s9Rnl0y06ZefwKtivDpykL7n3jn0CiqN11O20vpz02x9LwJADKksUOgtWh86M0IiSUtVwuAHPl7rxBph7Juc1m-vVQVCXdzraoEMC5CZaBjJxNaUOLNG5we6vBzxGEVlxbbjZoBZb5qe2_0FrbMsV6vR-sv2Ib8JcKdSPNkOgQHmebKofJfF3fa9PFxv3daymtH81qLWsvqevYrRiZ9_oE5od7N6dccYe7ilN53xJND4Jweg7U47yoI7_dpKgTXNUQuMWo0aMtJl-0joVDZYNarC7uazVKSg"
-	host := "104.156.140.42"
-	client := apis.NewWvpApi(model.WvpForm{
-		Server:   host,
-		Port:     18080,
-		ApiToken: apiToken,
-	})
-	//resp, err := client.GetDeviceList(context.Background(), 1, 10)
-	//resp := client.GetDeviceStatus(context.Background(), "44010200492000000001")
-	resp, _ := client.GetDeviceChannels(context.Background(), "44010200492000000001")
-	logrus.Debug(resp)
-
-	return
+	//apiToken := "eyJhbGciOiJSUzI1NiIsImtpZCI6IjNlNzk2NDZjNGRiYzQwODM4M2E5ZWVkMDlmMmI4NWFlIn0.eyJqdGkiOiJOZzZPR3BPeG03SGhhbzl6NlJYMmtRIiwiaWF0IjoxNzIzNzI4MzYxLCJleHAiOjE4NjE4OTExNDUsIm5iZiI6MTcyMzcyODM2MSwic3ViIjoibG9naW4iLCJhdWQiOiJBdWRpZW5jZSIsInVzZXJOYW1lIjoiYWRtaW4iLCJhcGlLZXlJZCI6MX0.ZkYCr1ffxij8wBHMCSJIqkeYoSAwe2ko-vVsnwi60SVxPA6lTosrv3s9Rnl0y06ZefwKtivDpykL7n3jn0CiqN11O20vpz02x9LwJADKksUOgtWh86M0IiSUtVwuAHPl7rxBph7Juc1m-vVQVCXdzraoEMC5CZaBjJxNaUOLNG5we6vBzxGEVlxbbjZoBZb5qe2_0FrbMsV6vR-sv2Ib8JcKdSPNkOgQHmebKofJfF3fa9PFxv3daymtH81qLWsvqevYrRiZ9_oE5od7N6dccYe7ilN53xJND4Jweg7U47yoI7_dpKgTXNUQuMWo0aMtJl-0joVDZYNarC7uazVKSg"
+	//host := "104.156.140.42"
+	//client := apis.NewWvpApi(model.WvpForm{
+	//	Server:   host,
+	//	Port:     18080,
+	//	ApiToken: apiToken,
+	//})
+	////resp, err := client.GetDeviceList(context.Background(), 1, 10)
+	////resp := client.GetDeviceStatus(context.Background(), "44010200492000000001")
+	//resp, _ := client.GetDeviceChannels(context.Background(), "44010200492000000001")
+	//logrus.Debug(resp)
+	//
+	//return
 	//初始化redis
 	cache.RedisInit()
 	// 启动mqtt客户端
