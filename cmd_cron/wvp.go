242687b1094a6e2e47090a8b2719cb34b47dc882
wangzongchun <zongchunw@gmail.com>
2024-08-17 16:39:22 +0800

    feat

--- cmd_cron/wvp.go
+++ cmd_cron/wvp.go
@@ -11,7 +11,7 @@ func StartInit() {
 	c := cron.New()
 
 	// 添加一个任务，使用 cmd_cron 表达式 "*/5 * * * *" 表示每 5 分钟执行一次
-	c.AddFunc("*/5 * * * *", func() {
+	c.AddFunc("*/1 * * * *", func() {
 		logrus.Debug("每 5 分钟执行一次任务:", time.Now())
 		services.NewWvpService().DeviceMqttPublish()
 	})
